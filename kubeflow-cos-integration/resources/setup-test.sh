7cc229f878e3f99f277f5a5d8a73590094b88ed3
Ivan Chvets <ivan.chvets@canonical.com>
2023-02-28 08:09:32 -0500

    doc: update README
    Summary of changes:
    - Minor updates.

--- kubeflow-cos-integration/resources/setup-test.sh
+++ kubeflow-cos-integration/resources/setup-test.sh
@@ -3,8 +3,14 @@
 #
 # NOTE: K8S cluster and Juju controller must be installed/deployed prior to running this test.
 #
+# install tools for testing
+sudo snap install jq
+
+# deploy Kubeflow
 juju add-model kubeflow
 juju deploy kubeflow --trust --channel latest/stable
+
+# deploy COS
 juju add-model cos
 curl -L https://raw.githubusercontent.com/canonical/cos-lite-bundle/main/overlays/offers-overlay.yaml -O
 juju deploy ./cos-lite-bundle.yaml --trust --overlay ./offers-overlay.yaml
